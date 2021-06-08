0212b61056078d4bb0e65af8b1512347d4769194
ciciliawidhiii <ciciliawidhi1@gmail.com>
2021-06-09 01:04:55 +0700

    new commit

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace eRent_Camera
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainMenu());
+            Application.Run(new LoginForm());
         }
     }
 }
